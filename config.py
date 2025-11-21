5fa7a8e1f591ab517017bcab263099a25eceeb7f
himaja pendyala <71513466+Himaja712@users.noreply.github.com>
2025-11-21 13:18:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-tenant_id = "842e4e0e-1c63-4147-b5bc-2b8baed2f998"
+tenant_id = ""
 client_id = "c5507ed5-6034-4b81-98e5-cd6a417ba598"
-client_secret = "o7v8Q~MLy0v0qIS05M.oDEbDmO3tNx789cRtpcCo"
+client_secret = ""
 
 # SharePoint Path connection
 site_hostname = "nicesoftwaresolutions1.sharepoint.com"
@@ -11,7 +11,7 @@ list_name="NestEmployeeList"
 # Database Connection
 host="localhost"
 user="root"
-password="training@123"
+password=""
 database="northwind"
 
 # Graph API connection
@@ -19,3 +19,4 @@ graph_api="https://graph.microsoft.com/v1.0/sites/"
 
 # Query/view to fetch data 
 query = "select * from employees WHERE employeestatus = 'Active' and FirstName not like '%Con%'"
+
